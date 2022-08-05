bc563d434221851a8641881061886d8fcf5f7ef9
Paula Santos <s.paula@ua.pt>
2022-08-05 09:52:20 +0100

    Go back

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import ManagementApp from "./components/ManagementApp";
+//import ManagementApp from "./components/ManagementApp";
 
 export default () => {
   return (
     <div>
       <h1>Hi there</h1>
       <hr />
-      <ManagementApp />
+      {/* <ManagementApp /> */}
     </div>
   );
 };
