434f5bc979714b250f75af90dd1299bbea515e5d
Paula Santos <s.paula@ua.pt>
2022-08-09 14:56:12 +0100

    Handle routing

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -5,6 +5,9 @@ const commonConfig = require("./webpack.common");
 
 const devConfig = {
   mode: "development",
+  output: {
+    publicPath: "http://localhost:8080/",
+  },
   devServer: {
     port: 8080,
     historyApiFallback: {
--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import ManagementApp from "./components/ManagementApp";
-import AuthApp from "./components/AuthApp";
 import Header from "./components/Header/Header";
 
 export default () => {
@@ -11,8 +10,6 @@ export default () => {
         <Header />
         <hr />
         <ManagementApp />
-        <hr />
-        <AuthApp />
       </div>
     </BrowserRouter>
   );
