8fb398fa51a1fe3810cee2b1ba5f0672ee94c1da
Paula Santos <s.paula@ua.pt>
2022-08-11 18:33:25 +0100

    Hide header for auth pages

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,11 +1,16 @@
 import React from "react";
 import Header from "./Header";
 import Routing from "../../config/Routing";
+import { useLocation } from "react-router-dom";
 
 const Layout = () => {
+  // TODO change this after doing login!
+  const location = useLocation();
+  const isLoggedIn = !location.pathname.includes("auth");
+
   return (
     <>
-      <Header />
+      {isLoggedIn ? <Header /> : null}
       <Routing />
     </>
   );
